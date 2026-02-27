44cc1e4aa7ba48c282177261586b398db2443ef9
HoussamLh <lahlahhoussam80@gmail.com>
2026-02-27 22:37:59 +0000

    arch: setup project page structure

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+function PortfolioPage() {
+  return (
+    <div>
+    <Navbar/>
+      <h1>Portfolio Page</h1>
+    <Footer />
+    </div>
+  );
+}
+export default PortfolioPage;
---
+++ app/services/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+function ServicesPage() {
+  return (
+    <div>
+    <Navbar/>
+      <h1>Services Page</h1>
+    <Footer />
+    </div>
+  );
+}
+export default ServicesPage;
