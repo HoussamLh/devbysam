e4503f39c7acea6e44b9ad99f268267278dc6d24
HoussamLh <lahlahhoussam80@gmail.com>
2026-02-28 01:36:44 +0000

    remove navbar + footer to avoid duplicate

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-
- function Home() {
+function Home() {
   return (
     <div>
-      <Navbar/>
       <h1>Home Page</h1>
-      <Footer />
     </div>
   );
 }
